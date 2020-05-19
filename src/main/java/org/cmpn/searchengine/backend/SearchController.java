151e381986811fdd81dc5e5a0b23d92e9317b18c
hzmmohamed <hzmohamed@gmail.com>
2020-05-20 01:16:35 +0200

    Enabled CORS

--- src/main/java/org/cmpn/searchengine/backend/SearchController.java
+++ src/main/java/org/cmpn/searchengine/backend/SearchController.java
@@ -1,5 +1,6 @@
 package org.cmpn.searchengine.backend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,11 @@ import java.util.ArrayList;
 @RestController
 public class SearchController {
 
-
+    @CrossOrigin
     @GetMapping("/search/web")
-    public  ArrayList<Document> results(@RequestParam(value = "query", defaultValue = "") String query,
+    public  ArrayList<Document> getResults(@RequestParam(value = "query", defaultValue = "") String query,
                                         @RequestParam(value = "page", defaultValue = "1") String page) {
-        ArrayList<Document> results = new ArrayList<Document>();
+        ArrayList<Document> results = new ArrayList<>();
         results.add(new Document("Wikipedia", "www.wikipedia.com", "This is wikipedia"));
         results.add(new Document("Brain Pickings", "www.brainpickings.com", "A decade of excellent writing"));
         results.add(new Document(query + " site", "www.query.com", "pageParam = " + page));
